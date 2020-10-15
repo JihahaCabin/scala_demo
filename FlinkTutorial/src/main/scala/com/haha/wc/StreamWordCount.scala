0f49baae54fb3e5c7633f8592430e906647ff971
zhengzhendu <zdzhengzhendu@163.com>
2020-10-15 22:56:14 +0800

    流处理 可以使用nc -lk 777 启动一个server侦听指定端口

---
+++ FlinkTutorial/src/main/scala/com/haha/wc/StreamWordCount.scala
@@ -0,0 +1,24 @@
+package com.haha.wc
+
+import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
+import org.apache.flink.streaming.api.scala._
+
+//流处理word count
+object StreamWordCount {
+
+  def main(args: Array[String]): Unit = {
+    //创建流处理的执行环境
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    //接收一个socket文本流
+    val inputDataStream = env.socketTextStream("192.168.255.168",7777)
+
+    //进行转换处理统计
+    val resultDataStream = inputDataStream.flatMap(_.split(" ")).filter(_.nonEmpty).map((_,1)).keyBy(0).sum(1)
+
+    resultDataStream.print()
+
+    //启动excutor,执行任务
+    env.execute("stream word count")
+  }
+}
