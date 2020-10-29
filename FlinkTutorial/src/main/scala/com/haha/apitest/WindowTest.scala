4907b3db6abb9adcc0da161296a29c598de0b458
zhengzhendu <zdzhengzhendu@163.com>
2020-10-29 22:48:41 +0800

    window api test  --- 读取socket数据

--- FlinkTutorial/src/main/scala/com/haha/apitest/WindowTest.scala
+++ FlinkTutorial/src/main/scala/com/haha/apitest/WindowTest.scala
@@ -1,17 +1,19 @@
 package com.haha.apitest
 
 import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
-import org.apache.flink.streaming.api.windowing.assigners.{SlidingEventTimeWindows, TumblingEventTimeWindows}
 import org.apache.flink.streaming.api.windowing.time.Time
 
 object WindowTest {
   def main(args: Array[String]): Unit = {
     // 创建一个批处理的执行环境
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setParallelism(1)
 
-    // 从文件中读取数据
-    val inputPath: String = "D:\\Flink\\MyDemo\\FlinkTutorial\\src\\main\\resources\\sensor.txt"
-    val stream = env.readTextFile(inputPath)
+    //    // 从文件中读取数据
+    ////    val inputPath: String = "D:\\Flink\\MyDemo\\FlinkTutorial\\src\\main\\resources\\sensor.txt"
+    ////    val stream = env.readTextFile(inputPath)
+
+    val stream = env.socketTextStream("192.168.255.188", 7777)
 
     //先转换为样例类
     val dataStream = stream.map(
@@ -21,18 +23,17 @@ object WindowTest {
       }
     )
 
-    val resultStream = dataStream.map(data => (data.id, data.temperature))
+    // 每15s统计一次，窗口各传感器所有温度的最小值，以及最新的时间戳
+    val resultStream = dataStream.map(data => (data.id, data.temperature, data.timeStamp))
       .keyBy(_._1) //按照二元组的第一个元素分组
       //   .window(TumblingEventTimeWindows.of(Time.seconds(15))) //定义15s的滚动时间窗口
       //   .window(SlidingEventTimeWindows.of(Time.seconds(15),Time.seconds(5))) //定义15s大小，5s间隔的滑动时间窗口
       .timeWindow(Time.seconds(15)) //简写方式
-      .reduce((r1, r2) => (r1._1, r1._2.max(r2._2)))
+      .reduce((cureRes, newData) => (cureRes._1, cureRes._2.min(newData._2), newData._3))
 
     resultStream.print()
 
     env.execute()
-
-
   }
 
 }
