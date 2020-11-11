80c819329c38adc2bb4bb6179f04b615afe66a27
zhengzhendu <zdzhengzhendu@163.com>
2020-11-11 23:38:51 +0800

    创建 Table 来描述文件数据，从文件中读取数据

--- FlinkTutorial/src/main/scala/com/haha/apitest/tabletest/TableApiTest.scala
+++ FlinkTutorial/src/main/scala/com/haha/apitest/tabletest/TableApiTest.scala
@@ -1,9 +1,11 @@
 package com.haha.apitest.tabletest
 
 import org.apache.flink.api.scala.ExecutionEnvironment
-import org.apache.flink.streaming.api.scala.StreamExecutionEnvironment
-import org.apache.flink.table.api.{EnvironmentSettings, TableEnvironment}
-import org.apache.flink.table.api.scala.{BatchTableEnvironment, StreamTableEnvironment}
+import org.apache.flink.streaming.api.scala.{StreamExecutionEnvironment, _}
+import org.apache.flink.table.api.{DataTypes, EnvironmentSettings, Table, TableEnvironment, _}
+import org.apache.flink.table.api.scala._
+import org.apache.flink.table.descriptors.{FileSystem, OldCsv, Schema}
+
 
 object TableApiTest {
   def main(args: Array[String]): Unit = {
@@ -13,33 +15,54 @@ object TableApiTest {
 
     val tableEnv = StreamTableEnvironment.create(env)
 
-    // 1.1 基于老版本planner的流处理
-    val settings = EnvironmentSettings.newInstance()
-      .useOldPlanner()
-      .inStreamingMode()
-      .build()
-    val oldStreamTableEnv = StreamTableEnvironment.create(env, settings)
+    /*    // 1.1 基于老版本planner的流处理
+        val settings = EnvironmentSettings.newInstance()
+          .useOldPlanner()
+          .inStreamingMode()
+          .build()
+        val oldStreamTableEnv = StreamTableEnvironment.create(env, settings)
 
-    // 1,2 基于老版本planner的批处理
-    val batchEnv = ExecutionEnvironment.getExecutionEnvironment
-    val oldBatchTableEnv = BatchTableEnvironment.create(batchEnv)
-
-
-    // 1.3 基于blink planner的流处理
-    val blinkStreamSettings = EnvironmentSettings.newInstance()
-      .useBlinkPlanner()
-      .inStreamingMode()
-      .build()
-
-    val blinkStreamTableEnv = StreamTableEnvironment.create(env, blinkStreamSettings)
+        // 1,2 基于老版本planner的批处理
+        val batchEnv = ExecutionEnvironment.getExecutionEnvironment
+        val oldBatchTableEnv = BatchTableEnvironment.create(batchEnv)
 
 
-    // 1.3 基于blink planner的批处理
-    val blinkBatchSettings = EnvironmentSettings.newInstance()
-      .useBlinkPlanner()
-      .inBatchMode()
-      .build()
+        // 1.3 基于blink planner的流处理
+        val blinkStreamSettings = EnvironmentSettings.newInstance()
+          .useBlinkPlanner()
+          .inStreamingMode()
+          .build()
 
-    val blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings)
+        val blinkStreamTableEnv = StreamTableEnvironment.create(env, blinkStreamSettings)
+
+
+        // 1.4 基于blink planner的批处理
+        val blinkBatchSettings = EnvironmentSettings.newInstance()
+          .useBlinkPlanner()
+          .inBatchMode()
+          .build()
+
+        val blinkBatchTableEnv = TableEnvironment.create(blinkBatchSettings)*/
+
+
+    // 2. 连接外部系统，读取数据，注册表
+
+    // 2.1 读取文件
+    val filePath = "D:\\Flink\\MyDemo\\FlinkTutorial\\src\\main\\resources\\sensor.txt";
+    tableEnv.connect(new FileSystem().path(filePath))
+      .withFormat(new OldCsv())
+      .withSchema(new Schema()
+        .field("id", DataTypes.STRING())
+        .field("timestamp", DataTypes.BIGINT())
+        .field("temperature", DataTypes.DOUBLE())
+      )
+      .createTemporaryTable("inputTable")
+
+    val inputTable: Table = tableEnv.from("inputTable")
+
+    inputTable.toAppendStream[(String, Long, Double)].print()
+
+    env.execute("table api test")
+
   }
 }
