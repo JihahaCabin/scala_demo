77cdc9c2353b59b46bcf4e940554c7a5f67c7e98
zhengzhendu <zdzhengzhendu@163.com>
2020-11-15 15:22:15 +0800

    使用.rowtime 可以定义事件时间属性

--- FlinkTutorial/src/main/scala/com/haha/apitest/tabletest/TimeAndWindowTest.scala
+++ FlinkTutorial/src/main/scala/com/haha/apitest/tabletest/TimeAndWindowTest.scala
@@ -5,13 +5,16 @@ import org.apache.flink.streaming.api.TimeCharacteristic
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.table.api.{DataTypes, EnvironmentSettings}
 import org.apache.flink.table.api.scala._
+import org.apache.flink.table.descriptors.{Csv, FileSystem, Schema}
 import org.apache.flink.types.Row
 
 object TimeAndWindowTest {
   def main(args: Array[String]): Unit = {
     val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setParallelism(1)
-    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    //    env.setStreamTimeCharacteristic(TimeCharacteristic.ProcessingTime)
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
+
 
     val settings = EnvironmentSettings.newInstance()
       .useBlinkPlanner()
@@ -19,7 +22,7 @@ object TimeAndWindowTest {
       .build()
     val tableEnv = StreamTableEnvironment.create(env)
 
-    // 从文件中读取数据
+    //1. 从文件中读取数据
     val inputPath: String = "D:\\Flink\\MyDemo\\FlinkTutorial\\src\\main\\resources\\sensor.txt"
     val stream = env.readTextFile(inputPath)
 
@@ -31,10 +34,14 @@ object TimeAndWindowTest {
       })
 
     //使用.proctime 定义处理时间
-    val sensorTable = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timeStamp, 'pt.proctime)
+    //    val sensorTable = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timeStamp, 'pt.proctime)
+    // 使用.rowtime 可以定义事件时间属性
+    val sensorTable = tableEnv.fromDataStream(dataStream, 'id, 'temperature, 'timeStamp.rowtime)
+
     //打印表结构
     sensorTable.printSchema()
     sensorTable.toAppendStream[Row].print()
+
 
     env.execute("time and window test")
 
